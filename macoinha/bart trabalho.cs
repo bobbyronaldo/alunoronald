663feb948586abee48b28672245498e092f15630
bobbyronaldo <bobbyronaldao@gmail.com>
2018-10-26 08:58:07 -0200

    Novo pojeto barth 25/10/18

---
+++ macoinha/bart trabalho.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaMediaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Neste projeto estarei desenvolvendo notas de um aluno e arredondando.
+            
+            string nome= "ronaldo";
+            double port=6.4, mat=7.6, ing=8.6, soma, media;
+            
+            // para informações aos comandos, usei double para declarar numeros quebrados e a varaivel somar, nmedia,e string pra textos e caracteries
+           
+            Console.WriteLine ("digite o nome do aluno"+nome);
+            
+          
+
+            
+        }
+    }
+}
